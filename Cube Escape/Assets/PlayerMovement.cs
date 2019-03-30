535dd69f2c807781cdc78c3831b11ae9f4ef751e
Arpit Rai <raisahab143@gmail.com>
2019-03-30 19:19:57 +0530

    Cube Escape - Version 1
    Cube Escape - Version 1

---
+++ Cube Escape/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    private GameObject playerBall;
+    // Use this for initialization
+	void Start () {
+        Debug.Log(" Game started ");
+        playerBall = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 startP = new Vector3(-0.09f,1.01f,-42.5f);
+        Vector3 endP = new Vector3(-0.09f,1.01f,-42.0f);
+        playerBall.transform.position = Vector3.Lerp(startP, endP, 1f);
+    }
+}
